0c6cecc7f32be22320a80fbaf3ac23f9a5ef1e27
Ruslan A <ruslan.akhmaev@outlook.com>
2023-01-17 23:50:17 +0700

    Добавил папки для домашних заданий

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 0. Демонстрация решения
+
+// Напишите программу, которая 
+// 1. на вход принимает число 
+// 2. и выдаёт его квадрат (число умноженное на само себя).
+// 3. Вывод результата
+
+// // Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine()); // were3454634
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
---
+++ Task01/Program.cs
@@ -0,0 +1,25 @@
+﻿// Итерация №1
+// Решение в группах задач:
+
+// 1. Напишите программу, которая на вход принимает два
+// числа и проверяет, является ли первое число квадратом
+// второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
+
+Console.WriteLine("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 / number2 == number2)
+{
+    Console.WriteLine($"Число {number1} является квадратом числа {number2}");
+}
+else
+{
+    Console.WriteLine($"Число {number1} не является квадратом числа {number2}");
+}
