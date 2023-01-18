f923fd32b0d8e8100e15fe4439580f68a1571112
Ruslan A <ruslan.akhmaev@outlook.com>
2023-01-18 15:08:27 +0700

    Решена Task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -8,3 +8,15 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3 
 */
+Console.Write("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 < number2)
+{
+    Console.WriteLine($"max = {number2}");
+}
+else
+{
+    Console.WriteLine($"max = {number1}");
+}
