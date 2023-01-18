c8830206d7ef30d9ac682a98e8a69b8ae9924d21
Ruslan A <ruslan.akhmaev@outlook.com>
2023-01-18 15:24:15 +0700

    Решена Task04

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -7,3 +7,22 @@
 44, 5, 78 -> 78
 22, 3, 9 -> 22 
 */
+Console.Write("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 1: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2 && number1 > number3)
+{
+    Console.WriteLine($"max = {number1}");
+}
+else if (number2 > number1 && number2 > number3)
+{
+    Console.WriteLine($"max = {number2}");
+}
+else
+{
+    Console.WriteLine($"max = {number3}");
+}
