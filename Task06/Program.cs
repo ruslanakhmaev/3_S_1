943a7588128931ef0401318bff3b25613de08cf8
Ruslan A <ruslan.akhmaev@outlook.com>
2023-01-18 15:30:11 +0700

    Решена Task06

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -8,3 +8,14 @@
 -3 -> нет
 7 -> нет 
 */
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine($"Число {number} является четным");
+}
+else
+{
+    Console.WriteLine($"Число {number} не является четным");
+}
